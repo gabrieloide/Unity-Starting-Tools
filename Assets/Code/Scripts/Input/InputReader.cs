f2c19188f080d449e4366ac2ab2e61d9c615542f
Gabriel <gabrielfuentes7312@gmail.com>
2026-05-15 20:51:12 -0400

    fix: comment out InputSystem_Actions to fix compiler error for new users

--- Assets/Code/Scripts/Input/InputReader.cs
+++ Assets/Code/Scripts/Input/InputReader.cs
@@ -10,7 +10,8 @@ namespace Code.Scripts.Input
     public class InputReader : BaseSingleton<InputReader>
     {
 #if ENABLE_INPUT_SYSTEM
-        private InputSystem_Actions _inputActions;
+        // [DESCOMENTAR DESPUÉS DE GENERAR LA CLASE C#]
+        // private InputSystem_Actions _inputActions;
 
         public static event Action<Vector2> OnMove;
         public static event Action OnJump;
@@ -20,6 +21,8 @@ namespace Code.Scripts.Input
         {
             base.Awake();
             
+            // [DESCOMENTAR DESPUÉS DE GENERAR LA CLASE C#]
+            /*
             _inputActions = new InputSystem_Actions();
             
             _inputActions.Player.Move.performed += ctx => OnMove?.Invoke(ctx.ReadValue<Vector2>());
@@ -27,18 +30,19 @@ namespace Code.Scripts.Input
             
             _inputActions.Player.Jump.performed += ctx => OnJump?.Invoke();
             _inputActions.Player.Interact.performed += ctx => OnInteract?.Invoke();
+            */
         }
 
         private void OnEnable()
         {
-            if (_inputActions != null)
-                _inputActions.Enable();
+            // [DESCOMENTAR DESPUÉS DE GENERAR LA CLASE C#]
+            // if (_inputActions != null) _inputActions.Enable();
         }
 
         private void OnDisable()
         {
-            if (_inputActions != null)
-                _inputActions.Disable();
+            // [DESCOMENTAR DESPUÉS DE GENERAR LA CLASE C#]
+            // if (_inputActions != null) _inputActions.Disable();
         }
 #else
         private void Awake()
